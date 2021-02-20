406a44e8bd6f7fce6d3dd449f1014de3b68b0b56
CrasyEnimal <druzini@gmail.com>
2021-02-20 13:27:07 +0300

    Face ready.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ values(10001, 'TaskDescription', false,'Task' );
 
 insert into tasks
 values(10002, 'TaskDescription2', false ,'Task2');
+
+insert into tasks
+values(10003, 'TaskDescription3', false ,'Task3');
+
+insert into tasks
+values(10004, 'TaskDescription4', false ,'Task4');
